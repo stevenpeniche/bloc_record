4e6105dec529ec8ac4b509d2c2689eee75aabbbc
Steven Peniche <stevenmpeniche@gmail.com>
2018-02-09 10:43:56 -0500

    Add 'Update'

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -205,7 +205,9 @@ module Selection
   end
 
   def rows_to_array(rows)
-    rows.map { |row| new(Hash[columns.zip(row)]) }
+    collection = BlocRecord::Collection.new
+    rows.each { |row| collection << new(Hash[columns.zip(row)]) }
+    collection
   end
 
   def validate_num(value)
