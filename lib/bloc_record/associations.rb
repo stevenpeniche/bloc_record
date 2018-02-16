ff85cbb85cff778f18df0309a18f85557d483c2d
Steven Peniche <stevenmpeniche@gmail.com>
2018-02-16 11:35:14 -0500

    Add 'PostgreSQL' Support

--- lib/bloc_record/associations.rb
+++ lib/bloc_record/associations.rb
@@ -2,6 +2,22 @@ require 'sqlite3'
 require 'active_support/inflector'
 
 module Associations
+  def has_one(association)
+    define_method(association) do
+      row = self.class.connection.execute <<-SQL
+        SELECT * FROM #{association.to_s}
+        WHERE #{self.class.table}_id = #{self.id}
+      SQL
+
+      class_name = association.to_s.classify.constantize
+
+      if row
+        data = Hash[class_name.columns.zip(row)]
+        class_name.new(data)
+      end
+    end
+  end
+
   def has_many(association)
     define_method(association) do
       rows = self.class.connection.execute <<-SQL
